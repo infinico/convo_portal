b051075c87b57a9c5b29d8e2f76d9b6e1256e2f8
Wakuza <pxy9548@rit.edu>
2015-06-25 14:04:31 -0400

    Added $linktoALL and $root andFixed the employment.php?force variable

--- HR/401K.php
+++ HR/401K.php
@@ -5,7 +5,28 @@
     include("../assets/inc/header.inc.php");
 ?>
 
-            <h1 class="headerPages">401(k)</h1>
+            <h1 class="headerPages">401(k) Savings &amp; Investment Plan</h1>
+
+            <h2>Overview</h2>
+
+            <p>Part-time and full-time employees are eligible to participate in the 401(k) Plan as long as they are over 21 years old.</p>
+
+<?php
+    $age = $user_data["date_of_birth"];
+    if($user_data["payroll_status"] == "FT" || $user_data["payroll_status"] == "PT"){
+        if((date("Y") - date("Y", strtotime($user_data["date_of_birth"]))) < 21){
+            echo "<p class='not_eligible'>Our records show that you are under 21 years of age. You will become eligible for participation in the 401(k) Plan after you turn 21.</p>";
+        }
+        else{
+            echo "<p class='eligible'>You are eligible for participation in the 401(k) Plan.</p>";
+        }
+    }
+    else{
+        echo "<p class='not_eligible'>You are not eligible for participation in the 401(k) Plan until you are employed as full-time or part-time.</p>";
+    }
+
+
+?>
 
             <p>Your 401(k) plan is provided by Kramer Financial Services. You can enroll in the 401(k) plan or make changes to: your salary contribution, fund selections, and transfers, or simply view your account balance via Paychex. If you have not registered for an account with Paychex, go to <a href="http://www.paychex.com/login/" target="_blank">http://www.paychex.com/login/</a> and register for Paychex Flex.</p> 
 
--- core/init.php
+++ core/init.php
@@ -1,5 +1,7 @@
 <?php
     $_ENV["HOSTNAME"] = "TESTING";
+    $linkToALL = "https://test.theinfini.com/convo";  
+    $root = realpath($_SERVER["DOCUMENT_ROOT"]);
 
     session_start();
  
@@ -8,7 +10,7 @@
     require "functions/general.php";
 
     $current_file = explode("/", $_SERVER["SCRIPT_NAME"]);
-    $current_file = end($current_file);
+    $current_file = "/convo/HR/" . end($current_file);
 
     if(logged_in() === true) {
         $session_user_id = $_SESSION['employee_id'];
@@ -28,6 +30,12 @@
         // This one forces the user to change the password when they click "forget the password"
         if($current_file !== "changepassword.php" && $current_file !== "logout.php" && $user_data["password_recover"] == 1) {
             header("Location: changepassword.php?force");
+            exit();
+        }
+        
+        // If the employees don't have email, it is being forced to the employment_data.php and fill out the e-mail address
+        if($current_file !== "/convo/HR/employment_data.php" && $current_file !== "logout.php" && $user_data["email"] == NULL) {
+            header("Location: $linkToALL/HR/employment_data.php?force");
             exit();
         }
     }
