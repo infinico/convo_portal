2230f1039d897fd9a583e08ef5c172d57d36035c
Wakuza <pxy9548@rit.edu>
2015-06-26 10:14:44 -0400

    Added Health Benefits page

---
+++ HR/HealthBenefits.php
@@ -0,0 +1,72 @@
+<?php
+    $title = "Convo Portal | Health Benefits";
+    include("../core/init.php");
+    protect_page();
+    include("../assets/inc/header.inc.php");
+?>
+            <h1 class="headerPages">Health Benefits</h1>
+            <ul class="hb_a_list">
+                <li><a href="#Medical">Medical</a> &nbsp; | &nbsp; <a href="#Dental">Dental</a> &nbsp; |&nbsp; <a href="#Vision">Vision</a></li>
+            </ul>
+
+            <h1 class="hb_header">Eligibility</h1>
+            <p>The effective date of coverage begins on the first of the month following 60 days of full-time, active and continuous employment.</p>
+
+            <h1 class="hb_header"><a name="Medical"></a>Medical</h1>
+
+            <h2 class="hb_header">Choice EPO</h2>
+            <p>The <strong>Choice EPO</strong> option gives you flexibility and control over your health care. Similar to an HMO, the Choice EPO gives you freedom to see any doctor in the UnitedHealthcare network, including Specialists, without getting a referral! Plus, when you visit network doctors and hospitals, there are no claim forms to complete or bills to worry about. Another advantage to the Choice EPO is that you can move to a different location without an interruption of service. With the open access feature of the Choice EPO option, you have access to any network physician throughout the country.</p>
+            <a class="hb_a_list" href="/benefits/PDF/SPD_UHC_Choice_EPO.pdf" target="_blank">EPO Summary Plan Description</a><br/><br/>
+
+            <h2 class="hb_header">Choice Plus PPO</h2>
+            <p>The <strong>Choice Plus PPO</strong> option gives you the flexibility you’ve already come to know with a PPO by allowing direct access to the network physicians or specialists without having to obtain a referral. The Choice Plus PPO option also enables you to seek services from non-network providers and still receive plan benefits. However, keep in mind that plan benefits are greater when you use network providers. In addition, you will be responsible for paying the remainder of expenses after the plan has paid its portion.</p>
+            <a class="hb_a_list" href="/benefits/PDF/SPD_UHC_Choice_Plus_PPO.pdf" target="_blank">PPO Summary Plan Description</a><br/><br/>
+
+            <h2 class="hb_header">Indemnity Plan</h2>
+            <p>The <strong>Indemnity Plan</strong> option provides maximum freedom for dealing with any health care situation. This incredibly flexible program lets you make your own health care decisions, including which doctors or specialists to visit. Benefits are provided for covered health services received from any physician or other licensed medical provider. When you receive covered health services from the national network of physicians, hospitals and other health care providers, you benefit from the contracted rates and fees that have been negotiated. <em>The Indemnity plan is only available in areas where there is no EPO or PPO Network available.</em></p>
+            <a class="hb_a_list" href="/benefits/PDF/SPD_UHC_Indemnity.pdf" target="_blank">Indemnity Summary Plan Description</a><br/><br/>
+
+            <h2 class="hb_header">Tools &amp; Resources</h2>
+            <ul class="hb_a_list">
+                <li><a href="/benefits/PDF/UHC_Medical_Plan_Overview.pdf" target="_blank">Medical Plan Overview</a></li>
+                <li><a href="/benefits/PDF/UHC_Medical_Claim_Form.pdf" target="_blank">UHC Medical Claim Form</a></li>
+                <li><a href="/benefits/PDF/2010 Benefits Program Guide - US.pdf" target="_blank">Benefits Program Guide</a></li>
+                <li><a href="https://www.myuhc.com/" target="_blank">My UHC Website</a></li>
+                <li><a href="http://www.medco.com" target="_blank">Medco Website</a></li>
+                <li><a href="MothersAndNewborns.aspx">Newborns and Mothers Health Protection Act</a></li>
+                <li><a href="WomensHealthAndCancerRights.aspx">Women’s Health and Cancer Rights Act</a></li>
+            </ul>
+
+            <h1 class="hb_header"><a name="Dental"></a>Dental</h1>
+            <p>Employees of Apollo Group, Inc. may choose from two dental plan options under the MetLife Dental program. These options are called Core and Premier Dental plan options. Network benefits in each of the two plans are paid at the same levels as shown in the chart located in Dental Plan Overview, under Tools &amp; Resources below. Out-of-Network benefit payments will vary according to the option you select, Core or Premier.</p>
+
+            <p><em>Note: The vision and dental premiums are bundled together. If you enroll in the dental plan, you will automatically be enrolled in the vision at the same coverage level you elected for dental.</em></p>
+
+            <h2 class="hb_header">Tools &amp; Resources</h2>
+            <ul class="hb_a_list">
+                <li><a href="/benefits/PDF/DentalPlanOverview.pdf" target="_blank">Dental Plan Overview</a></li>
+                <li><a href="/benefits/PDF/CoreDentalPlan.pdf" target="_blank">MetLife Core Dental Summary Plan Description</a></li>
+                <li><a href="/benefits/PDF/PremierDentalPlan.pdf" target="_blank">MetLife Premier Dental Summary Plan Description</a></li>
+                <li><a href="/benefits/PDF/MetlifeClaimForm.pdf" target="_blank">MetLife Dental Expense Claim Form</a></li>
+                <li><a href="http://www.metlife.com/dental" target="_blank">MetLife Website</a></li>
+                <li><a href="https://mybenefits.metlife.com/MyBenefits/" target="_blank">MetLife My Benefits Website</a></li>
+            </ul>
+
+            <h1 class="hb_header"><a name="Vision"></a>Vision</h1>
+            <p>Vision Service Plan (VSP) is a Preferred Provider Organization (PPO) plan with network doctors (Optometrists) that provide you with professional vision care and materials. Network doctors are those who belong to VSP’s Preferred Provider Organization.</p>
+
+            <p>Once you select an optometrist, simply make an appointment. The optometrist takes care of the rest – there are no claim forms to complete and benefits are verified by the optometrist at the time of service.</p>
+
+            <p><em>Note: The vision and dental premiums are bundled together. If you enroll in the dental plan, you will automatically be enrolled in the vision at the same coverage level you elected for dental.</em></p>
+
+            <h2 class="hb_header">Tools &amp; Resources</h2>
+            <ul class="hb_a_list">
+                <li><a href="/benefits/PDF/VSP_Overview.pdf" target="_blank">Vision Plan Overview</a></li>
+                <li><a href="/benefits/PDF/VSP_ValueAddedDiscount.pdf" target="_blank">Added Discount Program Addition</a></li>
+                <li><a href="/benefits/PDF/VisionServicePlan.pdf" target="_blank">Summary Plan Description</a></li>
+                <li><a href="http://www.vsp.com/" target="_blank">VSP Website</a></li>
+            </ul>
+
+<?php
+    include("../assets/inc/footer.inc.php");
+?>
