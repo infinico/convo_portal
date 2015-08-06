7d854468c2a9d2aeb18b9fb170bf7a18cae81bc0
Wakuza <pxy9548@rit.edu>
2015-08-06 10:59:05 -0400

    Add Incoming co-op student information

--- core/init.php
+++ core/init.php
@@ -1,8 +1,22 @@
 <?php
+    /* Environment Variables */
     $_ENV["HOSTNAME"] = "TESTING";
     $linkToALL = "https://test.theinfini.com/convo";  
     $root = realpath($_SERVER["DOCUMENT_ROOT"]);
 
+    /* 
+        INCOMING CO-OP STUDENT INFORMATION:
+        Please change the email address and name for testing.
+        The variables are used for email functions (PHPMailer)
+        If you have any questions, you can contact Josh at jja4740@rit.edu or Peter at pxy9548@rit.edu
+    */
+    $COOP1Email = 'jja4740@rit.edu';
+    $COOP1Name = 'Joshua Aziz';
+    $COOP2Email = 'pxy9548@rit.edu';
+    $COOP2Name = 'Peter Yeung';
+    $SupervisorCOOPEmail = 'chris@theinfini.com';
+    $SupervisorName = 'Chris Campbell';
+    
     session_start();
  
     require "database/connect.php";
