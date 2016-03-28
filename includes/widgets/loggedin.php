ffdc13d8d449e98f61eaddddd8be72a7dc661ecd
hardcorehiker <hardcorehiker@gmail.com>
2016-03-28 19:19:35 -0400

    Commented out "Change profile picture" link

--- includes/widgets/loggedin.php
+++ includes/widgets/loggedin.php
@@ -1,11 +1,14 @@
-                <div class="widget">
-                    <h2 id="hello">Hello <?php echo $user_data['firstname']; ?>!</h2>
-                    <div class="inner">
-                        <div id ="statusBox" class="block">
-                            <ul id = "status_logout">
-                                <li><a href="<?php echo $linkToALL; ?>/logout.php">Log out</a></li>
-                                <li><a href="<?php echo $linkToALL; ?>/changepassword.php">Change password</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
+<div class="widget">
+    <h2 id="hello">Hello <?php echo $user_data['firstname']; ?>!</h2>
+    <!--<p><?php echo $user_data['employee_id']; ?></h2>-->
+    <div class="inner">
+        <div id ="statusBox" class="block">
+            <ul id = "status_logout">
+                <li><a href="<?php echo $linkToALL; ?>/logout.php">Log out</a></li>
+                <li><a href="<?php echo $linkToALL; ?>/changepassword.php">Change password</a></li>
+                <!--<li><a href="<?php echo $linkToALL; ?>/changeProfilePic.php">Change Profile Picture</a></li>-->
+
+            </ul>
+        </div>
+    </div>
+</div>
