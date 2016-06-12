c1e5f934523be6d1f4251b81a45e51d5a1594372
hardcorehiker <hardcorehiker@gmail.com>
2016-06-12 18:53:54 -0500

    Announcement about Open Enrollment 2016

--- index.php
+++ index.php
@@ -11,8 +11,23 @@
 <?php
     if(logged_in() == true) {
 ?>
+    <p style="font-size: smaller; font-style: italic;">June 10, 2016</p>
+    <p>Hi everyone,<br /><br />
+    Open Enrollment is here!  We are pleased to announce that we were able to find a less costly plan with United Healthcare as opposed to what was offered through renewal of the current plans, to be effective July 1<sup>st</sup>.<br /><br />
+    Please review the following:<br /><br />
+    - <a href="Benefits/CH%20Insurance%20Brokerage%20Letter.pdf" target="_blank">Letter from our broker summarizing the change</a><br>
+    - <a href="Benefits/UHC%20Election%20Form.pdf" target="_blank">Election Form</a><br>
+    - <a href="Benefits/UHC%20-%20AGZP.pdf" target="_blank">Benefit Summary for Plan AGZP</a><br>
+    - <a href="Benefits/UHC%20-%20AGZY.pdf" target="_blank">Benefit Summary for Plan AGZY</a><br>
+    - <a href="Benefits/UHC%20-%20AGZZ.pdf" target="_blank">Benefit Summary for Plan AGZZ</a><br /><br />
+    The good news is if you are currently enrolled in a health plan, you will not need to fill out enrollment forms. Rather, the Election form can be filled out and returned to <a href="mailto:hr@convorelay.com">hr@convorelay.com</a>. Your individual and dependent information will automatically be transferred over.<br /><br />
+    As always, all Full Time employees are eligible to participate. Those who are Full Time and waiving coverage will need to inform HR via email that they are waiving coverage. All elections and waivers will need to be submitted on or before June 20<sup>th</sup>.<br /><br />
+    I want to thank everyone for their patience. Please don't hesitate to reach out to us if you have any questions or concerns. Thank you all for all you do for Convo!<br /><br />
+    HR</p>
+    <hr />
+
     <p style="font-size: smaller; font-style: italic;">June 8, 2016</p>
-    <div style="text-align: center;"><iframe width="600" height="337" src="https://www.youtube.com/embed/I5edLmtafAI" frameborder="0" allowfullscreen></iframe></div>
+    <div style="text-align: center;"><iframe width="600" height="337" src="https://www.youtube.com/embed/I5edLmtafAI?rel=0" frameborder="0" allowfullscreen></iframe></div>
     <p>Hello everyone,<br /><br />
     I hope this finds each of you well and happy. I am still floating from our amazing weekend in Rochester. We had an excellent turnout. It was such a pleasure to be there and feel the strong sense of community spirit in Rochester.<br /><br />
     As you know we are always looking for ways to improve our call centers and the experience and well being of our interpreters. In the past year we have had numerous improvements: Mobile, Rochester, Pleasanton and Roseville. Finally, Fort Wayne's time has come. Since day one Fort Wayne has been a heavy weight in our success. Numerous interpreters have faithfully commuted, some nearly two hours each way, from the Toledo area to serve in the Fort Wayne center. Now, we are excited to share and celebrate with you our plans to open two new centers in the region. Convo will have a new home in Fort Wayne and in Toledo.<br /><br />
@@ -27,7 +42,7 @@
     Jarrod</p>
     <hr />
     <p style="font-size: smaller; font-style: italic;">March 9, 2016</p>
-    <div style="text-align: center;"><iframe width="600" height="337" src="https://www.youtube.com/embed/6j7V3FJciI8" frameborder="0" allowfullscreen=""></iframe></div>
+    <div style="text-align: center;"><iframe width="600" height="337" src="https://www.youtube.com/embed/6j7V3FJciI8?rel=0" frameborder="0" allowfullscreen=""></iframe></div>
     <p>Folks,<br /><br />
     We turn seven years     old today! There have been some ups and downs, unexpected changes, and extraordinary growth along the way, but one thing has remained the same: our passion in elevating our Deaf community. We couldn't be any prouder of our work, and we're nowhere near done.<br /><br />
         Here's to many more birthdays to come!<br /><br />
@@ -45,7 +60,7 @@
     <hr />
 
     <p style="font-size: smaller; font-style: italic;">March 3, 2016</p>
-    <div style="text-align: center;"><iframe width="600" height="337" src="https://www.youtube.com/embed/Vv0q4_iCDvg" frameborder="0" allowfullscreen=""></iframe></div>
+    <div style="text-align: center;"><iframe width="600" height="337" src="https://www.youtube.com/embed/Vv0q4_iCDvg?rel=0" frameborder="0" allowfullscreen=""></iframe></div>
     <p>Folks,<br /><br />
     We have some great news to share!<br /><br />
     As you may remember, we've had some back-and-forth over the past few years with FCC regarding rate cuts. After repeated visits to FCC and thanks to all of you, our hard work has finally paid off and we can now breathe: FCC has ordered a rate-cut freeze for Tier-1 companies.<br /><br />
@@ -56,28 +71,28 @@
     <hr />
 
     <p style="font-size: smaller; font-style: italic;">February 16, 2016</p>
-    <div style="text-align: center;"><iframe width="600" height="337" src="https://www.youtube.com/embed/_9Jwbcf9oSM" frameborder="0" allowfullscreen=""></iframe></div>
+    <div style="text-align: center;"><iframe width="600" height="337" src="https://www.youtube.com/embed/_9Jwbcf9oSM?rel=0" frameborder="0" allowfullscreen=""></iframe></div>
     <p>Folks,<br /><br />
     Starting tomorrow, we will proceed with collecting the Social Security Numbers (SSNs) of our customers per FCC's updated policy requirement. Watch Jeff's video above for more information. It is important that we are all aware of this new requirement.<br/><br/>
     If you have any questions, feel free to contact Jeff at <a href="mailto:jeff@convorelay.com">jeff@convorelay.com</a>!</p>
     <hr />
 
     <p style="font-size: smaller; font-style: italic;">January 19, 2016</p>
-    <div style="text-align: center;"><iframe width="600" height="337" src="https://www.youtube.com/embed/IALED1g5nwY" frameborder="0" allowfullscreen=""></iframe></div>
+    <div style="text-align: center;"><iframe width="600" height="337" src="https://www.youtube.com/embed/IALED1g5nwY?rel=0" frameborder="0" allowfullscreen=""></iframe></div>
     <p>Folks,<br /><br />
     The first video of the year from Jarrod is here! Please take the time to watch his video.<br /><br />
         When sharing the two open job positions, please point to our postings at <a href="http://www.convorelay.com/jobs" target="_blank">convorelay.com/jobs</a>.<br/><br/>
         2016 is going to be a <strong>bold</strong> year. Here we go!</p>
     <hr />
     <p style="font-size: smaller; font-style: italic;">December 21, 2015</p>
-    <div style="text-align: center;"><iframe width="600" height="337" src="https://www.youtube.com/embed/pDYfmQDy_Xg" frameborder="0" allowfullscreen=""></iframe></div>
+    <div style="text-align: center;"><iframe width="600" height="337" src="https://www.youtube.com/embed/pDYfmQDy_Xg?rel=0" frameborder="0" allowfullscreen=""></iframe></div>
     <p>Hello everyone,<br /><br />
     This video by HR Generalist Isidore Niyongabo is a reminder to use your Convo work phone number when making work-related VRS calls. If you'd like to make personal VRS calls, you may log in your personal phone number. Please be careful to never mix the two to ensure confidentiality for your personal calls and because we are not allowed to bill to the FCC any minutes from Convo business-related VRS calls.<br /><br />
         Everyone must sign their acknowledgement of the revised policy on Convo Work Phone Number as indicated at the end of the page. You can find this under HR &gt; <a href="hr/acknowledgements.php">Acknowledgements</a> in the Convo portal.<br/><br/>
         If you have any questions, please contact HR at <a href="mailto:hr@convorelay.com">hr@convorelay.com</a>. Thank you!</p>
     <hr />
     <p style="font-size: smaller; font-style: italic;">November 30, 2015</p>
-    <div style="text-align: center;"><iframe width="600" height="337" src="https://www.youtube.com/embed/MbQaH2Q3Qs0" frameborder="0" allowfullscreen=""></iframe></div>
+    <div style="text-align: center;"><iframe width="600" height="337" src="https://www.youtube.com/embed/MbQaH2Q3Qs0?rel=0" frameborder="0" allowfullscreen=""></iframe></div>
     <p>Folks,<br /><br />We hope you had a nice time with your loved ones the past week. Thank you to those who submitted their gratitude videos. You might have seen our video of what Convo is grateful for on social media, but here's an extended version just for the company! Please enjoy the video above.<br /><br />You are what makes Convo such a special company. Thank you for being who you are.<br /><br />Love,<br />Convo</p>
     <hr />
     <p style="font-size: smaller; font-style: italic;">November 17, 2015</p>
@@ -88,24 +103,24 @@
     <p style="text-align:center"><b>FOR INTERNAL USE ONLY - DO NOT SHARE</b><br /><br /><a href="http://convorelay.us9.list-manage1.com/track/click?u=4b51a5c43c6b534eea0b32597&id=7ac85cfaec&e=ada3a74a64" target="_blank">Convo Organization Chart</a><br /><br /><a href="http://convorelay.us9.list-manage1.com/track/click?u=4b51a5c43c6b534eea0b32597&id=d36092a752&e=ada3a74a64" target="_blank">Access Interpreting Partnership</a></p>
     <hr />
     <p style="font-size: smaller; font-style: italic;">November 10, 2015</p>
-    <div style="text-align: center;"><iframe src="https://www.youtube.com/embed/dCjtoI9mr2Y" width="600" height="337" frameborder="0" allowfullscreen=""></iframe></div>
+    <div style="text-align: center;"><iframe src="https://www.youtube.com/embed/dCjtoI9mr2Y?rel=0" width="600" height="337" frameborder="0" allowfullscreen=""></iframe></div>
     <p>Folks,<br /><br />This is regarding our Customer Proprietary Network Information (CPNI) operating procedure and Privacy policy. The FCC has strict rules governing the use of CPNI, which relates to information about consumers' VRS calls. Our Privacy policy provides information to consumers about the information collected from them and how it is used.<br /><br />We require that all our employees read the attached CPNI operating procedure to ensure our full compliance with FCC's CPNI rules. We encourage you to read the attached Privacy policy, which is also posted on Convo's website, to be well informed in our consumer relations.<br /><br />Please contact your supervisor or Jeff Rosen if you have any questions.<br /><br />Thank you for your time!<br /><br /></p>
     <p style="text-align: center;"><a href="http://convorelay.us9.list-manage.com/track/click?u=4b51a5c43c6b534eea0b32597&amp;id=09d21360fa&amp;e=ada3a74a64" target="_blank">CPNI Procedures</a><br /><br /> <a href="http://convorelay.us9.list-manage2.com/track/click?u=4b51a5c43c6b534eea0b32597&amp;id=3eacd98881&amp;e=ada3a74a64" target="_blank">Convo's Privacy Policy</a></p>
     <hr />
     <p style="font-size: smaller; font-style: italic;">November 4, 2015</p>
-    <div style="text-align: center;"><iframe src="https://www.youtube.com/embed/evqpcYSBwOE" width="600" height="337" frameborder="0" allowfullscreen=""></iframe></div>
+    <div style="text-align: center;"><iframe src="https://www.youtube.com/embed/evqpcYSBwOE?rel=0" width="600" height="337" frameborder="0" allowfullscreen=""></iframe></div>
     <p>Folks,<br /><br /> We have another FCC update regarding a proposal that would pause the rate cuts for the three small providers, which includes Convo. If passed, this would be a good step for Convo to be able to uphold the quality of service and amount of innovation we have been able to provide thus far. Watch Jeff's vlog for more information and contact Jeff if you have any questions.</p>
     <p style="text-align: center;"><a href="http://convorelay.us9.list-manage1.com/track/click?u=4b51a5c43c6b534eea0b32597&amp;id=cefac4aa7b&amp;e=ada3a74a64" target="_blank">Read the Further Notice of Proposed Rule Making here.</a></p>
     <p>Keep up the good work, everyone!</p>
     <hr />
     <p style="font-size: smaller; font-style: italic;">September 1, 2015</p>
-    <div style="text-align: center;"><iframe src="https://www.youtube.com/embed/J66I6UAYiy8?list=PLwR2tgM-4_AYEr5MDfhdb_AsfLZF4Upwi" width="600" height="337" frameborder="0" allowfullscreen=""></iframe></div>
+    <div style="text-align: center;"><iframe src="https://www.youtube.com/embed/J66I6UAYiy8?list=PLwR2tgM-4_AYEr5MDfhdb_AsfLZF4Upwi&rel=0" width="600" height="337" frameborder="0" allowfullscreen=""></iframe></div>
     <p>Folks,<br /><br />Jeff has some updates to share with us regarding three topics:</p>
     <p>The recent rate cut<br /> Social Security Number requirements<br /> Maintaining transparency within the company</p>
     <p>If you have any questions, please feel free to email Jeff at <a href="mailto:jeff@convorelay.com">jeff@convorelay.com</a>.</p>
     <hr />
     <p style="font-size: smaller; font-style: italic;">August 18, 2015</p>
-    <div style="text-align: center;"><iframe src="https://www.youtube.com/embed/aGLhTSjVoEE" width="600" height="337" frameborder="0" allowfullscreen=""></iframe></div>
+    <div style="text-align: center;"><iframe src="https://www.youtube.com/embed/aGLhTSjVoEE?rel=0" width="600" height="337" frameborder="0" allowfullscreen=""></iframe></div>
     <p>Folks,&nbsp;<br /><br />We could not be any more thrilled to introduce&nbsp;Convo TV, the first hardware product to come from Convo! It is currently available only in school edition and the first school to experience Convo TV is California School for the Deaf, Fremont.<br /><br />One of the many&nbsp;special things about Convo TV is that it is also the world's first Emergency/Public Announcements system designed for the Deaf.&nbsp;On top of that, we have recently made direct&nbsp;access to Spanish VRS possible with Convo TV, through a partnership with another VRS provider.&nbsp;<br /><br />This is only the beginning of an exciting journey with Convo TV! Watch the videos below to learn more.&nbsp;</p>
     <p style="text-align: center;"><a href="http://convorelay.us9.list-manage.com/track/click?u=4b51a5c43c6b534eea0b32597&amp;id=e51b170db1&amp;e=b7f4a6ebf4" target="_blank">Convo TV Introduction Video</a><br /><br /> <a href="http://convorelay.us9.list-manage.com/track/click?u=4b51a5c43c6b534eea0b32597&amp;id=3d1bc6a557&amp;e=b7f4a6ebf4" target="_blank">Spanish VRS Announcement</a></p>
 <?php
