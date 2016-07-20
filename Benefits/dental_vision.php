d28e8b71e7152693d604a908a8d3d68031f732fc
hardcorehiker <hardcorehiker@gmail.com>
2016-07-20 09:05:06 -0500

    Added links to dental and vision care provider networks

--- Benefits/dental_vision.php
+++ Benefits/dental_vision.php
@@ -13,13 +13,15 @@ if(logged_in()){
 
 <h2>Eligibility</h2>
 
-<p>Dental and vision insurance is available through HealthEdge to all full-time employees. If full-time status begins on the first of the month, the effective date of coverage begins on that date. Otherwise, the effective date of coverage begins on the first of the month following full-time employment status.</p>
+<p>Dental and vision insurance are available through HealthEdge to all full-time employees. If full-time status begins on the first of the month, the effective date of coverage begins on that date. Otherwise, the effective date of coverage begins on the first of the month following full-time employment status.</p>
+
+<p>Enrolled employees in California have access to the <a href="https://www.firstdentalhealth.com/" target="_blank">First Dental Health network</a>, while enrolled employees outside of California have access to the <a href="http://www.connectiondental.com/" target="_blank">Connection Dental network</a>.</p>
+
+<p>Vision care providers can be found on the <a href="https://www.eyemed.com/" target="_blank">EyeMed web site</a>.</p>
 
 <h2>Coverage</h2>
 
-<p>Dental and vision insurance coverage is at 100% the employee's cost with pre-tax deductions from your 
-
-    paycheck. </p>
+<p>Dental and vision insurance coverage is at 100% the employee's cost with pre-tax deductions from your paycheck. </p>
 
 <h2>Dental/Vision Insurance Plan Documents</h2>
 
