2a198ec6d30178e65a1f128b54334fea44ac9b54
hardcorehiker <hardcorehiker@gmail.com>
2016-10-15 00:02:18 -0500

    onboarding_report updated with NEO checklist
    updated with neo checklist

--- Admin/onboarding_report.php
+++ Admin/onboarding_report.php
@@ -1,35 +1,96 @@
 <?php 
-    $page_title = "Onboarding Report";
-    $title = "Convo Portal | Onboarding Report";
+    $page_title = "Onboarding Status";
+    $title = "Convo Portal | Onboarding Checklist";
+    require_once "../includes/phpmailer/vendor/autoload.php";
+    require("../includes/phpmailer/libs/PHPMailer/class.phpmailer.php");
     include("../core/init.php");
     manager_protect();
     include("../assets/inc/header.inc.php");
+
+
+    //$resultemployee = mysqli_query($link, "SELECT * FROM employee_info_vw");
+        
+        $query = "SELECT * FROM employee_info_vw JOIN neo_tracking WHERE neo_tracking.employee_id = employee_info_vw.employee_id";
+
+        
+        $result = mysqli_query($link, $query);
+
+        $num_rows = mysqli_affected_rows($link);
+
+    
+    if(isset($_POST["submit"])) {
+        
+       //$backgroundcheck_query = "SELECT * FROM employee_info_vw WHERE employee_id = '" . $_POST['backgroundChecked$i'] . "'";
+       //$supervisor_query= 
+        
+       // echo "NUM: " . $num_rows;
+        for($i=1; $i < $num_rows; $i++){
+        
+           // echo $i . ": " . isset($_POST["backgroundChecked$i"]);
+                
+            if(isset($_POST["backgroundChecked$i"])){
+                
+                $name =                     
+                //$supervisor = "SELECT supervisor_id FROM employee_info_vw";
+            
+                $employee_id = sanitize($_POST["backgroundChecked$i"]);
+
+                mysqli_query($link, "CALL update_neo_background('$employee_id', 'Y')") or die($link);
+
+                onboarding_checklist_notification($name, $supervisor);
+
+                echo "<h2 class='headerPages'>The employee checklist was updated successfully!</h2>";
+
+
+                die();
+
+            } 
+            
+          
+        }
+    }
 ?>
 
-        <h1 class="headerPages">New Employee Onboarding Report</h1>
+<h1 class="headerPages">New Employee Checklist</h1>
+
+<form id= "neo_form" action="onboarding_report.php" method="post">
+    <?php
+        
+$i = 1;
+        echo "<table id='onboarding_table' class='display' cellspacing='0' width='1010px'>";
+            if ($result && $num_rows > 0) { 
+               echo "<thead><tr><th style='background-color:#71AB3A'>First Name</th>" .
+                   "<th style='background-color:#71AB3A'>Last Name</th>" .
+                   "<th style='background-color:#71AB3A'>Status</th>" .
+                   "<th style='background-color:#71AB3A'>Hire Date</th>" .
+                   "<th style='background-color:#71AB3A'>Background Check Cleared</th>" .
+
+
+                   "</tr></thead><tbody>";
+                while ($row = mysqli_fetch_assoc($result)) {
+                    
+                    $i++;
+                    
+                    if($row["bkgd_clear"] != "Y"){
+                    
+                        echo "<tr><td>" . $row["firstname"] . "</td><td>" . $row["lastname"] . "</td><td>" . $row["checklist_status"]  . "</td><td>" . $row["hire_date"] . "</td><td><input type='checkbox' name='backgroundChecked$i' value='" . $row["employee_id"] . "'>";
+                    }
+                    else{
+                        echo "<tr><td>" . $row["firstname"] . "</td><td>" . $row["lastname"] . "</td><td>" . $row["checklist_status"]  . "</td><td>" . $row["hire_date"] . "</td><td><input type='checkbox' checked='checked' disabled='disabled' name='backgroundChecked$i' value='" . $row["employee_id"] . "'>";              
+                    }
+
+                }
+            }        
+        echo "</tbody></table>";
+
+?>
+
+    <input type="submit" id="submit" name="submit" value="Update">
+
+</form>
 
 
 <?php
-    $query = "SELECT * FROM employee_info_vw WHERE emp_type = 'O' ORDER BY updated_at DESC";
-    $result = mysqli_query($link, $query);
-    
-    $num_rows = mysqli_affected_rows($link);
-    echo "<table id='onboarding_table' class='display' cellspacing='0' width='1010px'>";
-        if ($result && $num_rows > 0) { 
-           echo "<thead><tr><th style='background-color:#71AB3A'>First Name</th>" .
-               "<th style='background-color:#71AB3A'>Last Name</th>" .
-               "<th style='background-color:#71AB3A'>City</th>" .
-               "<th style='background-color:#71AB3A'>State</th>" .
-               "<th style='background-color:#71AB3A'>Email</th>" .
-               "<th style='background-color:#71AB3A'>Updated At</th>" .
-               "<th style='background-color:#71AB3A'>Status</th>" .
 
-               "</tr></thead><tbody>";
-            while ($row = mysqli_fetch_assoc($result)) {
-                echo "<tr><td>" . $row["firstname"] . "</td><td>" . $row["lastname"] . "</td><td>" . $row["city"]  . "</td><td>" . $row["res_state"] . "</td><td><a target=_blank href= mailto:" . $row["email"] . ">" . $row["email"] . "</a></td><td>" . $row["updated_at"] . "</td><td>Background Check</td></tr>";
-
-            }
-        }        
-    echo "</tbody></table>";
     include("../assets/inc/footer.inc.php");
 ?>
