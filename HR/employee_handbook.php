380105c2474c2e012f19722bca70d4378105a134
hardcorehiker <hardcorehiker@gmail.com>
2016-10-27 10:34:16 -0500

    created employee_handbook.php

---
+++ HR/employee_handbook.php
@@ -0,0 +1,38 @@
+<?php
+    $page_title = "Convo Employee Handbook 2016";
+    $title = "Convo Employee Handbook 2016";
+    include("../core/init.php");
+    protect_page();
+    include("../assets/inc/header.inc.php");
+?>
+
+            <h1 class="headerPages">Convo Employee Handbook 2016</h1>
+            <br />
+            <div id="colLeft">    <!-- column Left -->
+                <!-- Employee Resources -->
+              
+                <ul class="resources">
+                    
+                    <li><a href="Handbook/ConvoEmployeeHandbook%202016%20Edition.pdf" target="_blank"> Convo Employee Handbook 2016 </a></li>
+
+                    <!--<li><a href="Handbook/Handbook%20-%20Welcome.pdf" target="_blank">Welcome</a></li>
+                    <li><a href="Handbook/Handbook%20-%20Foreword.pdf" target="_blank">Foreword</a></li>
+                    <li><a href="Handbook/Handbook%20-%20General%20Employment%20Policies.pdf" target="_blank">General Employment Policies</a></li>
+                    <li><a href="Handbook/Handbook%20-%20Business%20Ethics%20and%20Conduct.pdf" target="_blank">Business Ethics and Conduct</a></li>
+                    <li><a href="Handbook/Handbook%20-%20Attendance.pdf" target="_blank">Attendance</a></li>
+                    <li><a href="Handbook/Handbook%20-%20Safety.pdf" target="_blank">Safety</a></li>
+                    <li><a href="Handbook/Handbook%20-%20Employment%20Status,%20Performance,%20Wage,%20and%20Hours%20Policies.pdf"
+                           target="_blank">Employment Status, Performance, Wage, and Hour Policies</a></li>
+                    <li><a href="Handbook/Handbook%20-%20Employee%20Benefit%20Programs.pdf" target="_blank">Employment Benefit Programs</a></li>
+                    <li><a href="Handbook/Handbook%20-%20Workplace%20Expectations%20and%20Miscellaneous%20Company%20Policies.pdf" target="_blank">Workplace Expectations and Miscellaneous Company Policies</a></li>
+                    <li><a href="Handbook/Handbook%20-%20Acknowledgement%20of%20Receipt%20of%20Handbook.pdf" target="_blank">Acknowledgement of Receipt of Handbook</a></li>
+                    <li><a href="Handbook/Handbook%20-%20Appendix%20I.pdf" target="_blank">Appendix I</a></li> -->
+
+                    
+
+                </ul><br/>
+            </div>
+
+<?php
+    include("../assets/inc/footer.inc.php");
+?>
