8eea1b9b2b0db0e292781729db13bddbd5f7abcf
tturner0 <tturner0@users.noreply.github.com>
2016-10-27 01:46:22 -0400

    able to handle the single quotation marks

--- Admin/files_uploaded.php
+++ Admin/files_uploaded.php
@@ -1,34 +1,41 @@
 <?php
-    $page_title = "Files Uploaded";
-    $title = "Convo Portal | File Uploaded";
-    include("../core/init.php");
-    admin_protect();
-    protect_page();
-    include("../assets/inc/header.inc.php");
 
-    // Bytes Conversion Function
-    function formatBytes($size, $precision = 2) {
-        $base = log($size, 1024);
-        $suffixes = array('', 'KB', 'MB', 'GB', 'TB');   
+$page_title = "Files Uploaded";
 
-        return round(pow(1024, $base - floor($base)), $precision) . " " . $suffixes[floor($base)];
-    }
+$title = "Convo Portal | File Uploaded";
 
-    // Loop through checkboxes to see which files should be deleted    
-    if(isset($_POST["submit"])) 
+include("../core/init.php");
+
+admin_protect();
+
+protect_page();
+
+include("../assets/inc/header.inc.php");
+
+// Bytes Conversion Function
+
+function formatBytes($size, $precision = 2) {
+    $base = log($size, 1024);
+    $suffixes = array('', 'KB', 'MB', 'GB', 'TB');
+    return round(pow(1024, $base - floor($base)), $precision) . " " . $suffixes[floor($base)];
+}
+
+// Loop through checkboxes to see which files should be deleted
+
+if(isset($_POST["submit"]))
+{
+    if(!empty($_POST['fileToDelete']))
     {
-        if(!empty($_POST['fileToDelete'])) 
+        foreach($_POST['fileToDelete'] as $file)
         {
-            foreach($_POST['fileToDelete'] as $file) 
-            {
-                $file = str_replace("Admin/", "", $file);
-                unlink($file);
-            }
+            $file = str_replace("Admin/", "", $file);
+            unlink($file);
         }
     }
+}
 ?>
 
-            <script>
+<script>
             function confirmDelete()
             {
                 var chkCount = 0;
@@ -38,6 +45,7 @@
                         chkCount++;
                     }
                 }
+
                 if (chkCount == 0) {
                     alert("You did not select any files to delete.");
                     return false;
@@ -45,44 +53,50 @@
                     return confirm("Are you sure you want to proceed with deleting the selected files?");
                 }
             }
-            </script>
+</script>
 
-            <h1 class="headerPages">Files Uploaded to Portal</h1>
+<h1 class="headerPages">Files Uploaded to Portal</h1>
 
-            <h2>Files</h2>
+<h2>Files</h2>
 
-            <form id="files" name="files" action="files_uploaded.php" method="POST" onsubmit="return confirmDelete();">
-            <div class="file_upload">   <!-- File Upload -->
-                <table class="table table-bordered table-hover" id="tab_logic">
-                    <thead>
-                        <tr>
-                            <th>File</th>
-                            <th>Date</th>
-                            <th>Size</th>
-                            <th>Delete?</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-            <?php
+<form id="files" name="files" action="files_uploaded.php" method="POST" onsubmit="return confirmDelete();">
+    <div class="file_upload">
+        <!-- File Upload -->
+        <table class="table table-bordered table-hover" id="tab_logic">
+            <thead>
+                <tr>
+                    <th>File</th>
+                    <th>Date</th>
+                    <th>Size</th>
+                    <th>Delete?</th>
+                </tr>
+            </thead>
+            <tbody>
 
-                function newest($a, $b) { 
-                    return (filemtime($a) > filemtime($b)) ? -1 : 1; 
-                } 
+                <?php
 
-                $dir = glob($root . '/convo/Admin/upload_oe/*'); // put all files in an array 
+                function newest($a, $b) {
+                    return (filemtime($a) > filemtime($b)) ? -1 : 1;
+                }
+
+                $dir = glob($root . '/convo/Admin/upload_oe/*'); // put all files in an array
+
                 //$dir = glob($root . '/convo/Admin/upload_oe/*');
-                uasort($dir, "newest"); // sort the array by calling newest() 
 
-                foreach($dir as $file) { 
-                    $fileLink = substr($file, 25);
-                    echo "<tr><td width='30%'><a href='" . $linkToALL . "/" . $fileLink .  "' target='_blank'>" . basename($file) . "</a></td><td width='30%'>" . date("F d, Y g:i:s A", filemtime($file)) . "</td><td width='30%'>" .  formatBytes(filesize($file), 1) . "</td><td width='10%'><input type='checkbox' name='fileToDelete[]' value='" . $fileLink . "'></td></tr>";
-                } 
-            ?>         
-                    </tbody>
-                </table>
-            </div>  <!-- File Upload // -->
-            <input type="submit" id="FMLAButton" name="submit" value="Delete Selected Files">
-            </form>
+                uasort($dir, "newest"); // sort the array by calling newest()
+
+                foreach($dir as $file) {
+                    $fileLink = substr($file, 22);
+                    echo '<tr><td width="30%"><a href="'.$linkToALL.'/'. $fileLink.'" target="_blank">' . basename($file) . '</a></td><td width="30%">' . date("F d, Y g:i:s A", filemtime($file)) . '</td><td width="30%">' .  formatBytes(filesize($file), 1) . '</td><td width="10%"><input type="checkbox" name="fileToDelete[]" value="' . $fileLink . '"></td></tr>';
+                }
+                ?>
+            </tbody>
+        </table>
+    </div>
+    <!-- File Upload // -->
+    <input type="submit" id="FMLAButton" name="submit" value="Delete Selected Files" />
+</form>
+
 <?php
-    include("../assets/inc/footer.inc.php");
+include("../assets/inc/footer.inc.php");
 ?>
